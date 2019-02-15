aa6a4b1d389830d82d74b8e5953d9e2677526eaa
Bryt Xin Li <bryt.xin.li@gmail.com>
2019-02-15 17:34:55 +0800

    update

--- startup.sh
+++ startup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-export LDAP_ADMIN_DN="cn=admin,dc=redin,dc=co"
-export LDAP_ADMIN_PASSWORD="admin"
-export LDAP_CONFIG_PASSWORD="config"
-
-export SMTP_SERVER=
-export SMTP_SERVER_PORT=
-export SMTP_ENCRYPTION=
-export SMTP_USER=
-export SMTP_PASS=
-
+LDAP_ADMIN_DN="cn=admin,dc=redin,dc=co" \
+LDAP_ADMIN_PASSWORD="admin" \
+LDAP_CONFIG_PASSWORD="config" \
+\
+SMTP_SERVER= \
+SMTP_SERVER_PORT= \
+SMTP_ENCRYPTION= \
+SMTP_USER= \
+SMTP_PASS= \
 docker stack deploy -c docker-compose.yml devops
